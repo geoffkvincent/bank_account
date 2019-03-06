72e918201d504fb95a58e8534184b41289c1250b
Geoff Vincent <geoffkvincent@gmail.com>
2019-03-06 16:44:12 -0700

    menu and switch func

--- my_bank.py
+++ my_bank.py
@@ -1,20 +1,22 @@
+import time
 account = 0
 
 class MyBank:
+
     def __init__(self):
-        print('Welcome to My Bank')
-        print('------------------')
+        print('--------$$$ Welcome to MyBank $$$-----------')
+        print('------------------------------------------')
         print("It looks like you don't have an account with us.")
         print("Would you like to open one today?  (y/n) ")
         user_input = input()
         if user_input == 'y':
             self.getAmountFromConsole()
         elif user_input == 'n':
-            sleep(2)
+            time.sleep(2)
             exit()
         else:
             print('Invalid Input')
-            sleep(2)
+            time.sleep(2)
             MyBank()
 
     def getAmountFromConsole(self): 
@@ -22,6 +24,32 @@ class MyBank:
         user_input = eval(input('Enter Amount: $'))
         account = user_input
         print('Your current balance is: $%d ' % account)
+
+    def menu(self):
+        print('------------------------')
+        print('Please make a selection:')
+        print('------------------------')
+        print('1) Make a Deposit ')
+        print('2) Make a Withdrawl')
+        print('3) Current Balance')
+        print('4) Exit')
+        user_input = eval(input())
+        self.menu_switch(user_input)
+        
+    def menu_switch(self, user_input):
+        if user_input == 1:
+            self.deposit()
+        elif user_input == 2:
+            self.withdraw()
+        elif user_input == 3:
+            self.balance()
+        elif user_input == 4:
+            exit()
+        else:
+            print('Invalid Input')
+            time.sleep(2)
+            self.menu()
+        
 
     def deposit(self):
         global account
@@ -38,11 +66,12 @@ class MyBank:
         print('Your current balance is %d' % account)
 
 bank = MyBank()
-bank.getAmountFromConsole()
-print(account)
-bank.deposit()
-print(account)
-bank.withdraw()
-print(account)
-bank.balance()
+bank.menu()
+# bank.getAmountFromConsole()
+# print(account)
+# bank.deposit()
+# print(account)
+# bank.withdraw()
+# print(account)
+# bank.balance()
 
